0f7a2a5ce100f47ab1eea85a470a8e2d5117be9e
VeluruPavankumar <113927931+VeluruPavankumar@users.noreply.github.com>
2025-03-17 14:12:11 +0530

    Update Login.js

--- reviewfeapp/src/auth/Login.js
+++ reviewfeapp/src/auth/Login.js
@@ -28,6 +28,7 @@ const Login = () => {
       <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required />
       <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
       <button type="submit">Login</button>
+      <button onClick={() => navigate("/signup")}>Sign Up</button>
     </form>
   );
 };
